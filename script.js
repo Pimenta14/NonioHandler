05591ca985db94af2f2361e7a528afd13905b42e
Pedro Ribeiro (ext.) <Pedro.Ribeiro@ctw.bmwgroup.com>
2019-03-25 17:55:23 +0000

    Initial Commit

---
+++ script.js
@@ -0,0 +1,203 @@
+var target = "";
+var nonioCalls = 0;
+var cookiesCalls = 0;
+var successFlag = 0;
+
+removeCookies();
+removeNonio();
+
+function removeNonio() {
+	if (document.URL.indexOf("aquelamaquina.pt") >= 0) {
+		target = ["layer_gatting", "sso_layer"];
+		removeIdNONIO(target);	
+	} 
+
+	if (document.URL.indexOf("autoportal.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	} 
+
+	if (document.URL.indexOf("classificadoscm.pt") >= 0) {
+		target = ["sso_layer"];
+		removeIdNONIO(target);
+	}
+
+	if (document.URL.indexOf("cmjornal.pt") >= 0) {
+		target = ["layer_gatting", "sso_layer"];
+		removeIdNONIO(target);
+	} 
+
+	if (document.URL.indexOf("dinheirovivo.pt") >= 0) {
+		var iframes = document.querySelectorAll('iframe');
+		for (var i = 0; i < iframes.length; i++) {
+			iframes[i].parentNode.removeChild(iframes[i]);
+		}
+	} 
+
+	if (document.URL.indexOf("dn.pt") >= 0) {
+		target = ["tp-modal", "tp-backdrop tp-active"];
+		removeClassNONIO(target);
+	} 
+
+	if (document.URL.indexOf("expresso.pt") >= 0) {
+		target = ["imp-content-gate-root"];
+		removeIdNONIO(target);
+	}
+
+	if (document.URL.indexOf("flash.pt") >= 0) {
+		target = ["layer_gatting"];
+		removeIdNONIO(target);
+	}
+
+	if (document.URL.indexOf("jn.pt") >= 0) {
+		target = ["tp-modal", "tp-backdrop tp-active"];	
+		removeClassNONIO(target);
+	} 
+
+	if (document.URL.indexOf("jornaldenegocios.pt") >= 0) {
+		target = ["layer_gatting", "sso_layer"];	
+		removeIdNONIO(target);
+	} 
+
+	if (document.URL.indexOf("maisfutebol.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	}
+
+	if (document.URL.indexOf("maxima.pt") >= 0) {
+		target = ["layer_gatting", "sso_layer"];
+		removeIdNONIO(target);
+	} 
+
+	if (document.URL.indexOf("megahits.sapo.pt") >= 0) {
+		target = ["wrapperContentGatingNonio"];
+		removeIdNONIO(target);
+	}
+
+	if (document.URL.indexOf("motor24.pt") >= 0) {
+		var iframes = document.querySelectorAll('iframe');
+		for (var i = 0; i < iframes.length; i++) {
+			iframes[i].parentNode.removeChild(iframes[i]);
+		}
+	} 
+
+	if (document.URL.indexOf("noticiasmagazine.pt") >= 0) {
+		var iframes = document.querySelectorAll('iframe');
+		for (var i = 0; i < iframes.length; i++) {
+			iframes[i].parentNode.removeChild(iframes[i]);
+		}
+	} 
+
+	if (document.URL.indexOf("radiocomercial.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	} 
+
+	if (document.URL.indexOf("selfie.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	}
+
+	if (document.URL.indexOf("smoothfm.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	} 
+
+	if (document.URL.indexOf("tvi.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	}
+
+	if (document.URL.indexOf("tvi24.iol.pt") >= 0) {
+		target = ["nonioBox"];
+		removeClassNONIO(target);
+	} 
+
+	if (document.URL.indexOf("record.pt") >= 0) {
+		target = ["layer_gatting"];
+		removeIdNONIO(target);
+	} 
+
+	if (document.URL.indexOf("ojogo.pt") >= 0) {
+		target = ["tp-modal", "tp-backdrop tp-active"];	
+		removeClassNONIO(target);
+	};
+};
+
+function removeClass(elementName) {
+	var elem = document.getElementsByClassName(elementName);
+	elem[0].remove();
+};
+
+function removeClassNONIO(targetList) {
+	console.log("Remove NONIO tentatives = " + nonioCalls);
+	document.body.style.cssText = 'overflow:auto !important';
+	document.getElementsByTagName('html')[0].style.overflow = "auto";
+	nonioCalls = nonioCalls + 1;
+	var i;
+	for (i = 0; i < targetList.length; i++) {
+		try {
+			removeClass(targetList[i]);
+			successFlag = successFlag + 1;
+		} catch (error) {
+			console.log("Error catched || " + error);
+		}
+	}
+
+	if (successFlag >= targetList.length) {
+		nonioCalls = 0;
+		successFlag = 0;
+	} else {
+		if (nonioCalls < 5) {
+			setTimeout(function() {
+				removeClassNONIO(targetList);
+			}, 500);
+		}	
+	}	
+};
+
+function removeIdNONIO(targetList) {
+	console.log("Remove NONIO tentatives = " + nonioCalls);
+	document.body.style.cssText = 'overflow:auto !important';
+	document.getElementsByTagName('html')[0].style.overflow = "auto";
+	
+	nonioCalls = nonioCalls + 1;
+	var i;
+	for (i = 0; i < targetList.length; i++) {
+		try {
+			document.getElementById(targetList[i]).remove();
+			successFlag = successFlag + 1;
+		} catch (error) {
+			console.log("Error catched || " + error);
+		}
+	}
+	
+	if (successFlag >= targetList.length) {
+		nonioCalls = 0;
+		successFlag = 0;
+	} else {
+		if (nonioCalls < 5) {
+			setTimeout(function() {
+				removeIdNONIO(targetList);
+			}, 500);
+		}
+	}
+};
+
+function removeCookies() {
+	console.log("Clean cookies calls = " + cookiesCalls);
+	document.body.style.cssText = 'overflow:auto !important';
+	document.getElementsByTagName('html')[0].style.overflow = "auto";
+	cookiesCalls = cookiesCalls + 1;
+	try {
+		removeClass("qc-cmp-ui-container qc-cmp-showing");
+		cookiesCalls = 0;
+	} catch (error) {
+			console.log("Error catched || " + error);
+		if (cookiesCalls < 5) {
+			setTimeout(function() {
+				removeCookies();
+			}, 500);
+		}
+	}
+}
