1155610b0f42925d3b22d93977aa16b1b5136da1
Pedro Ribeiro (ext.) <Pedro.Ribeiro@ctw.bmwgroup.com>
2019-03-27 19:10:54 +0000

    added activeTab permission to manifest fixed a bug where removeCookies function were removing important sections in other websites. now only removing for wanted websites

--- script.js
+++ script.js
@@ -3,31 +3,35 @@ var nonioCalls = 0;
 var cookiesCalls = 0;
 var successFlag = 0;
 
-removeCookies();
 removeNonio();
 
 function removeNonio() {
 	if (document.URL.indexOf("aquelamaquina.pt") >= 0) {
+		removeCookies();
 		target = ["layer_gatting", "sso_layer"];
 		removeIdNONIO(target);	
 	} 
 
 	if (document.URL.indexOf("autoportal.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	} 
 
 	if (document.URL.indexOf("classificadoscm.pt") >= 0) {
+		removeCookies();
 		target = ["sso_layer"];
 		removeIdNONIO(target);
 	}
 
 	if (document.URL.indexOf("cmjornal.pt") >= 0) {
+		removeCookies();
 		target = ["layer_gatting", "sso_layer"];
 		removeIdNONIO(target);
 	} 
 
 	if (document.URL.indexOf("dinheirovivo.pt") >= 0) {
+		removeCookies();
 		var iframes = document.querySelectorAll('iframe');
 		for (var i = 0; i < iframes.length; i++) {
 			iframes[i].parentNode.removeChild(iframes[i]);
@@ -35,46 +39,55 @@ function removeNonio() {
 	} 
 
 	if (document.URL.indexOf("dn.pt") >= 0) {
+		removeCookies();
 		target = ["tp-modal", "tp-backdrop tp-active"];
 		removeClassNONIO(target);
 	} 
 
 	if (document.URL.indexOf("expresso.pt") >= 0) {
+		removeCookies();
 		target = ["imp-content-gate-root"];
 		removeIdNONIO(target);
 	}
 
 	if (document.URL.indexOf("flash.pt") >= 0) {
+		removeCookies();
 		target = ["layer_gatting"];
 		removeIdNONIO(target);
 	}
 
 	if (document.URL.indexOf("jn.pt") >= 0) {
+		removeCookies();
 		target = ["tp-modal", "tp-backdrop tp-active"];	
 		removeClassNONIO(target);
 	} 
 
 	if (document.URL.indexOf("jornaldenegocios.pt") >= 0) {
+		removeCookies();
 		target = ["layer_gatting", "sso_layer"];	
 		removeIdNONIO(target);
 	} 
 
 	if (document.URL.indexOf("maisfutebol.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	}
 
 	if (document.URL.indexOf("maxima.pt") >= 0) {
+		removeCookies();
 		target = ["layer_gatting", "sso_layer"];
 		removeIdNONIO(target);
 	} 
 
 	if (document.URL.indexOf("megahits.sapo.pt") >= 0) {
+		removeCookies();
 		target = ["wrapperContentGatingNonio"];
 		removeIdNONIO(target);
 	}
 
 	if (document.URL.indexOf("motor24.pt") >= 0) {
+		removeCookies();
 		var iframes = document.querySelectorAll('iframe');
 		for (var i = 0; i < iframes.length; i++) {
 			iframes[i].parentNode.removeChild(iframes[i]);
@@ -82,6 +95,7 @@ function removeNonio() {
 	} 
 
 	if (document.URL.indexOf("noticiasmagazine.pt") >= 0) {
+		removeCookies();
 		var iframes = document.querySelectorAll('iframe');
 		for (var i = 0; i < iframes.length; i++) {
 			iframes[i].parentNode.removeChild(iframes[i]);
@@ -89,36 +103,43 @@ function removeNonio() {
 	} 
 
 	if (document.URL.indexOf("radiocomercial.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	} 
 
 	if (document.URL.indexOf("selfie.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	}
 
 	if (document.URL.indexOf("smoothfm.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	} 
 
 	if (document.URL.indexOf("tvi.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	}
 
 	if (document.URL.indexOf("tvi24.iol.pt") >= 0) {
+		removeCookies();
 		target = ["nonioBox"];
 		removeClassNONIO(target);
 	} 
 
 	if (document.URL.indexOf("record.pt") >= 0) {
+		removeCookies();
 		target = ["layer_gatting"];
 		removeIdNONIO(target);
 	} 
 
 	if (document.URL.indexOf("ojogo.pt") >= 0) {
+		removeCookies();
 		target = ["tp-modal", "tp-backdrop tp-active"];	
 		removeClassNONIO(target);
 	};
